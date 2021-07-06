a7c739a6bbdc44c55ad0e63b4d6aaaf2235117ed
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-06 12:11:12 +0000

    [Finish]4-3

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,6 +1,5 @@
 class PostImagesController < ApplicationController
   
-  
   def new
     @post_image = PostImage.new
   end
@@ -13,18 +12,21 @@ class PostImagesController < ApplicationController
   end
   
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.all 
   end
   
   def show
+    @post_image = PostImage.find(params[:id])
   end
   
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
   
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9caa102a1eada21e9a885b8b331532f0040a663b5404cc4d4a7b0c9e83be0fc39c7902caf6c2bffd62977d9d72fba697921ed547ccc900ea52c7c91b156134b6'
